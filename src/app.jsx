00cc0bacc1d9a4729cd8dc889b398b84d70e6589
midezen <victordiya90@gmail.com>
2024-05-29 18:15:33 +0100

    project setup

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div``;
+
+const App = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Navbar = () => {
+  return <Container>This is a navbar</Container>;
+};
+
+export default Navbar;
